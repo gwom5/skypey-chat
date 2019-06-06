611e2112fb615a5da4dedf606f13442c9ecb49d4
Chomo Gwom <chomogwom@gmail.com>
2019-06-06 03:56:09 +0200

    Added a menu on each message

--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -1,20 +1,54 @@
 import React, { Component } from "react";
 import "./Chats.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown} from '@fortawesome/free-solid-svg-icons';
 
-const Chat = ({ message }) => {
-    const { text, is_user_msg } = message;
-    let isClicked = false;
-
-    const messageIsClicked =()=>{
-        console.log("Message clicked");
-        isClicked = !isClicked;
+class  Chat extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isHovered:false
+        }
+    
+        this.messageHover = this.messageHover.bind(this);
     }
+     messageHover (){
+        this.setState(prevState => ({
+            isHovered: !prevState.isHovered
+        })); 
+        
+    }
+    //wrap the span element within a div, alongside the font awesome icon. Div could be a flex item with row direction to avoid text moving
+    render(){
+        const {text, is_user_msg} = this.props.message;
+        const hoverClass = this.state.isHovered? "hover": "";
+        if(!this.state.isHovered === true){
+            return (
+
+                <span className={`Chat ${is_user_msg ? "is-user-msg" : "" } ${hoverClass}`}  onMouseEnter={this.messageHover} onMouseLeave ={this.messageHover}>{text} </span>
+            );
+        }
+        else{
+            return (
+                <span className={`Chat ${is_user_msg ? "is-user-msg" : "" } ${hoverClass}`}  onMouseEnter={this.messageHover} onMouseLeave ={this.messageHover}>
+                    {text} 
+                    <MenuIcon top = {true}/>   
+                </span>
+            );
+        }
+        
+    }
+}
+
+
+const MenuIcon =()=>{
 
     return (
-        <span className={`Chat ${is_user_msg ? "is-user-msg" : "" }`}  onClick={messageIsClicked}>{text} </span>
+        <FontAwesomeIcon icon ={faAngleDown} />
     );
-};
 
+
+}
 
 class Chats extends Component {
     constructor(props){
