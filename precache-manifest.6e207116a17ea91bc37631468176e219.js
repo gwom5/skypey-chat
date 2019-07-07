2f48da9cb030b15c284fe77e983dacfc459d4049
Chomo Gwom <chomogwom@gmail.com>
2019-07-07 22:58:44 +0200

    Updates

---
+++ precache-manifest.6e207116a17ea91bc37631468176e219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339379a8c7f7161260918168469ce4c1",
+    "url": "/skypey-chat/index.html"
+  },
+  {
+    "revision": "a386f362c0ddb6c55cea",
+    "url": "/skypey-chat/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3b15c2c56d1af972782a",
+    "url": "/skypey-chat/static/css/main.a2032624.chunk.css"
+  },
+  {
+    "revision": "a386f362c0ddb6c55cea",
+    "url": "/skypey-chat/static/js/2.7d4fea10.chunk.js"
+  },
+  {
+    "revision": "3b15c2c56d1af972782a",
+    "url": "/skypey-chat/static/js/main.ddf11677.chunk.js"
+  },
+  {
+    "revision": "7af27d8eb0be4706845c",
+    "url": "/skypey-chat/static/js/runtime~main.f6736c11.js"
+  }
+]);
